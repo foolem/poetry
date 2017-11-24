f5d88c866fbd82e3739db8962e806b637f9eb052
Filipe Fenrich <foolemdev@gmail.com>
2017-11-24 20:34:15 -0200

    deu por hoje

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,5 @@
 $("#poems_anchor").click(function() {
-   $('html,body').animate({scrollTop: $('#poems').offset().top}, 1000);
+   $('html,body').animate({scrollTop: $('#poems').offset().top}, 1000, function(){
+     $('#presentation').remove();
+   });
 });
