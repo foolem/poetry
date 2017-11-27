71d45c2745109df338abb0426ef9fd42dfbb40c8
Filipe Fenrich <foolemdev@gmail.com>
2017-11-27 17:40:05 -0200

    first deploy

--- config.inc.php
+++ config.inc.php
@@ -3,7 +3,7 @@ session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 try {
-  $PDO = new PDO( 'mysql:host=' . 'localhost' . ';dbname=' . 'trab', 'root', 'admin', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+  $PDO = new PDO( 'mysql:host=' . 'mysql762.umbler.com' . ';dbname=' . 'trab', 'foolem', 'ursopolar1', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
   $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch ( PDOException $e ) {
