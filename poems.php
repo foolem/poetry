1c3a6a69a01e409e1533151f45be342dbcdbe768
barbara <babarbaravb@gmail.com>
2017-11-28 17:28:07 -0200

    testeantessem modal

--- poems.php
+++ poems.php
@@ -35,12 +35,12 @@ include_once "index.inc.php";
           <a class="login-btn" href="logout.php">Logout</a>
           <?php else: ?>
 
-            <button type="button" id="new_user" class="btn btn-primary mx-2" data-toggle="modal" data-target="#signup-modal">
+            <a href="new_user.php" id="new_user" class="btn btn-primary mx-2">
               Criar conta
-            </button>
-            <button type="button" id="login" class="btn btn-primary mx-2" data-toggle="modal" data-target="#login-modal">
+            </a>
+            <a href="login.php" id="login" class="btn btn-primary mx-2" >
               Login
-            </button>
+            </a>
 
           <?php endif; ?>
 
