a97def0bb190cd695dca011745e9faa34ddbef3c
barbara <babarbaravb@gmail.com>
2017-11-28 14:54:14 -0200

    vai

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ include_once "index.inc.php";
     <title>Sua Poesia</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/application.css">
 
   </head>
@@ -195,11 +194,15 @@ include_once "index.inc.php";
       </div>
     </div>
 
+    <footer class="footer">
+      <span>Bárbara Vidal | Felipe Polchlopek | Filipe Fenrich </span><br>
+      <span> <a href="user_terms.php">Termos de uso</a> </span>
+    </footer>
+
     <script
     src="https://code.jquery.com/jquery-3.2.1.min.js"
     integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
     crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script src="assets/js/application.js" type="text/javascript"></script>
   </body>
--- poems.php
+++ poems.php
@@ -187,11 +187,15 @@ include_once "index.inc.php";
       </div>
     </div>
 
+    <footer class="footer">
+      <span>Bárbara Vidal | Felipe Polchlopek | Filipe Fenrich </span><br>
+      <span> <a href="user_terms.php">Termos de uso</a> </span>
+    </footer>
+
     <script
     src="https://code.jquery.com/jquery-3.2.1.min.js"
     integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
     crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script src="assets/js/application.js" type="text/javascript"></script>
   </body>
